3b23068b83f07a2e48390268b131dcc873f33559
Khani1824 <khanimambomaluleke@gmail.com>
2024-11-07 13:06:48 +0200

    Implementation of the Fibonacci sequence values in a method

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -8,7 +8,30 @@ public class  Fibonacci {
         Scanner input = new Scanner(System.in);
         int position = input.nextInt();
 
+        // Fibonacci computation
+        int value = fibo(position);
+
 
     }
 
+    public static Integer fibo( int nthValue){
+        // Initializing an array to store the fibonacci values that will be computed
+        int [] sequence  = new int [nthValue];
+
+        // Manually computing the first wo values of the
+        // Fibonacci sequence in order to preform the calculation of the rest of the numbers
+        sequence[0] = 0;
+        sequence[1] = 1;
+
+        //Creating for loop to continue to calculate fibonacci value until position provided
+        // For Loop starts at index position 2 because the first two positions have already been defined
+        int i;
+        for(i = 2; i < nthValue; i++){
+
+            // Fibonacci sequence formula (e.g Fn = Fn-1 + Fn -2)
+            sequence[i] = sequence[i -1] + sequence[i -2];
+        }
+
+        return sequence[i - 1];
+    }
 }

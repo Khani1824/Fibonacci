d2b1909cba8c4cc402bb817a4f630926bde700e5
Khani1824 <khanimambomaluleke@gmail.com>
2024-11-07 12:45:50 +0200

    Input the position of the value user is looking for

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class  Fibonacci {
+
     public static void main(String[] args) {
-        System.out.println("Hello This code will, compute a dynamic programming (DP) of Fibonacci sequence");
+        // Compute the nth number
+        System.out.println("Enter the value of the position you looking for");
+        Scanner input = new Scanner(System.in);
+        int position = input.nextInt();
+
+
     }
+
 }
